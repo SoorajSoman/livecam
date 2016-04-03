cd2c8b02e6436af424ef668ef4bd47db27f09f0d
soorajksp <soorajsoman22@gmail.com>
2016-04-03 22:51:57 +0530

    Initial commit

---
+++ app/src/main/java/nv/android/com/livecam/AppConfig.java
@@ -0,0 +1,8 @@
+package nv.android.com.livecam;
+
+public class AppConfig {
+	public static final String STREAM_URL = "rtsp://192.168.1.4:1935/live/myStream";
+	//public static final String STREAM_URL = "rtsp://192.168.1.4:1935/Webapp/wowza_web_app";
+	public static final String PUBLISHER_USERNAME = "Jacob";
+	public static final String PUBLISHER_PASSWORD = "1234";
+}
